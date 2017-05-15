2af9303b06f9124197f4908f5b4618eb17639dc1
srhande <srhande@gmail.com>
2017-05-15 10:44:01 -0700

    added toast messages

--- MyContactApp/app/src/main/java/com/example/handes6253/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/handes6253/mycontactapp/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.handes6253.mycontactapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,10 +32,22 @@ public class MainActivity extends AppCompatActivity {
         if (isInserted == true){
             Log.d("MyContact", "Success inserting data");
             //Insert Toast message here
+            Context context = getApplicationContext();
+            CharSequence text = "Success";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
         }
         else {
             Log.d("MyContact", "Failure inserting data");
             //Insert toast message
+            Context context = getApplicationContext();
+            CharSequence text = "Fail";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
         }
     }
 }
