c61179ddad29309223477170f7aca30d760ca5e9
srhande <srhande@gmail.com>
2017-05-15 10:30:57 -0700

    addData/insertData methods

--- MyContactApp/app/src/main/java/com/example/handes6253/mycontactapp/DatabaseHelper.java
+++ MyContactApp/app/src/main/java/com/example/handes6253/mycontactapp/DatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.example.handes6253.mycontactapp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -28,5 +29,15 @@ public class DatabaseHelper extends SQLiteOpenHelper{
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+
+    public boolean insertData(String name) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, name);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if (result == -1) return false;
+        else return true;
     }
 }
--- MyContactApp/app/src/main/java/com/example/handes6253/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/handes6253/mycontactapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.handes6253.mycontactapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -17,5 +19,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myDb = new DatabaseHelper(this);
+
+        //Add the layout vars
+        editName = (EditText) findViewById(R.id.editText_name);
+    }
+
+    public void addData(View v){
+        boolean isInserted = myDb.insertData(editName.getText().toString());
+
+        if (isInserted == true){
+            Log.d("MyContact", "Success inserting data");
+            //Insert Toast message here
+        }
+        else {
+            Log.d("MyContact", "Failure inserting data");
+            //Insert toast message
+        }
     }
 }
